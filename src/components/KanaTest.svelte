456ff69711280302c62928327c09f318a11eb45d
Alan Le <36429067+mikumino@users.noreply.github.com>
2024-02-26 19:20:51 -0800

    feat: kana selectioin

---
+++ src/components/KanaTest.svelte
@@ -0,0 +1,19 @@
+<script>
+    import KanaSelection from './kana-test/KanaSelection.svelte'; 
+    let inTest = false;
+    function startTest(kana) {
+        console.log(kana);
+        inTest = true;
+    }
+</script>
+
+<div class="m-6 max-w-4xl md:mx-auto">
+    <h1 class="text-4xl font-bold mb-4">Kana Test</h1>
+    <p class="mb-4">This is a test to see if you can recognize the different kana characters. Select which kana you want to be tested on below.</p>
+    <p class="mb-4">This is only meant to help you remember the kana. If you don't know what they are at all, I'd recommend looking at <a class="link" href="https://www.tofugu.com/japanese/learn-hiragana/">Tofugu's guides</a>.</p>
+    {#if inTest}
+        <p>Test</p>
+    {:else}
+        <KanaSelection onSubmit={startTest} />
+    {/if}
+</div>
---
+++ src/components/kana-test/KanaSelection.svelte
@@ -0,0 +1,90 @@
+<script>
+    export let onSubmit;
+    let mainKanaRow = [
+        { id: 1, name: 'あいうえお', selected: false },
+        { id: 2, name: 'かきくけこ', selected: false },
+        { id: 3, name: 'さしすせそ', selected: false },
+        { id: 4, name: 'たちつてと', selected: false },
+        { id: 5, name: 'なにぬねの', selected: false },
+        { id: 6, name: 'はひふへほ', selected: false },
+        { id: 7, name: 'まみむめも', selected: false },
+        { id: 8, name: 'やゆよ', selected: false },
+        { id: 9, name: 'らりるれろ', selected: false },
+        { id: 10, name: 'わをん', selected: false }
+    ]
+
+    let dakutenRow = [
+        { id: 11, name: 'がぎぐげご', selected: false },
+        { id: 12, name: 'ざじずぜぞ', selected: false },
+        { id: 13, name: 'だぢづでど', selected: false },
+        { id: 14, name: 'ばびぶべぼ', selected: false },
+        { id: 15, name: 'ぱぴぷぺぽ', selected: false }
+    ]
+
+    let combinationRow = [
+        { id: 16, name: 'きゃきゅきょ', selected: false },
+        { id: 17, name: 'しゃしゅしょ', selected: false },
+        { id: 18, name: 'ちゃちゅちょ', selected: false },
+        { id: 19, name: 'にゃにゅにょ', selected: false },
+        { id: 20, name: 'ひゃひゅひょ', selected: false },
+        { id: 21, name: 'みゃみゅみょ', selected: false },
+        { id: 22, name: 'りゃりゅりょ', selected: false }
+    ]
+
+    function getSelectedKana() {
+        let selectedKana = [];
+        mainKanaRow.forEach(row => {
+            if (row.selected) {
+                selectedKana.push(row.name);
+            }
+        });
+        dakutenRow.forEach(row => {
+            if (row.selected) {
+                selectedKana.push(row.name);
+            }
+        });
+        combinationRow.forEach(row => {
+            if (row.selected) {
+                selectedKana.push(row.name);
+            }
+        });
+        return selectedKana;
+    }
+
+    function startTest() {
+        onSubmit(getSelectedKana());
+    }
+</script>
+
+<div class="flex flex-col w-full">
+    <div class="grid grid-cols-3 gap-4">
+        <div class="flex flex-col items-center">
+            <h2 class="text-2xl font-bold mb-4">Main Kana</h2>
+            {#each mainKanaRow as row}
+                <label for={row.id} class={`text-xl btn btn-primary ${row.selected ? '' : 'btn-outline'} mb-2 w-full`}>
+                    <input type="checkbox" id={row.id} bind:checked={row.selected} class="hidden" />
+                    {row.name}
+                </label>
+            {/each}
+        </div>
+        <div class="flex flex-col items-center">
+            <h2 class="text-2xl font-bold mb-4">Dakuten</h2>
+            {#each dakutenRow as row}
+                <label for={row.id} class={`text-xl btn btn-primary ${row.selected ? '' : 'btn-outline'} mb-2 w-full`}>
+                    <input type="checkbox" id={row.id} bind:checked={row.selected} class="hidden" />
+                    {row.name}
+                </label>
+            {/each}
+        </div>
+        <div class="flex flex-col items-center">
+            <h2 class="text-2xl font-bold mb-4">Combination</h2>
+            {#each combinationRow as row}
+                <label for={row.id} class={`text-xl btn btn-primary ${row.selected ? '' : 'btn-outline'} mb-2 w-full`}>
+                    <input type="checkbox" id={row.id} bind:checked={row.selected} class="hidden" />
+                    {row.name}
+                </label>
+            {/each}
+        </div>
+    </div>
+    <button on:click={startTest} class="btn btn-primary mt-4">Start Test</button>
+</div>
