4b1cf91919ed4e7e973862d3b89528f7b75000f1
Alan Le <36429067+mikumino@users.noreply.github.com>
2024-02-26 19:58:24 -0800

    basic quiz funtionality

--- src/components/KanaTest.svelte
+++ src/components/KanaTest.svelte
@@ -7,6 +7,9 @@
         selectedKana = kana;
         inTest = true;
     }
+    function stopTest() {
+        inTest = false;
+    }
 </script>
 
 <div class="m-6 max-w-4xl md:mx-auto">
@@ -14,7 +17,7 @@
     <p class="mb-4">This is a test to see if you can recognize the different kana characters. Select which kana you want to be tested on below.</p>
     <p class="mb-4">This is only meant to help you remember the kana. If you don't know what they are at all, I'd recommend looking at <a class="link" href="https://www.tofugu.com/japanese/learn-hiragana/">Tofugu's guides</a>.</p>
     {#if inTest}
-        <Quiz selectedKana={selectedKana} />
+        <Quiz selectedKana={selectedKana} stopTest={stopTest} />
     {:else}
         <KanaSelection onSubmit={startTest} />
     {/if}
