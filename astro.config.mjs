56cf842d23aae34082606cbd04239519805b1f42
Alan Le <36429067+mikumino@users.noreply.github.com>
2024-07-01 16:00:18 -0700

    shadcn init

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), mdx(), svelte()]
+  integrations: [react(), tailwind({
+    applyBaseStyles: false,
+  }),, mdx(), svelte()]
 });
