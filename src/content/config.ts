af55499a368d46aeadbc0e23a4ef3d347209e19b
Alan Le <36429067+mikumino@users.noreply.github.com>
2025-01-22 17:52:33 -0800

    add resources collection schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,27 +1,46 @@
-import { z, defineCollection } from 'astro:content';
+import { z, defineCollection } from "astro:content";
 
 const guidesCollection = defineCollection({
-    type: 'content',
-    schema: ({ image }) => z.object({
-        title: z.string(),
-        description: z.string(),
-        image: image(),
-        imageAlt: z.string()
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      image: image(),
+      imageAlt: z.string(),
     }),
 });
 
 const postsCollection = defineCollection({
-    type: 'content',
-    schema: ({ image }) => z.object({
-        title: z.string(),
-        description: z.string(),
-        date: z.date(),
-        image: image(),
-        imageAlt: z.string()
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      date: z.date(),
+      image: image(),
+      imageAlt: z.string(),
+    }),
+});
+
+const resourcesCollection = defineCollection({
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string().optional(),
+      image: image(),
+      imageAlt: z.string(),
+      url: z.string().optional(),
+      inUse: z.boolean().optional(),
+      recommended: z.boolean().optional(),
+      category: z.string().optional(),
+      tags: z.array(z.string()).optional(),
     }),
 });
 
 export const collections = {
-    'personal-guides': guidesCollection,
-    'blog': postsCollection
+  "personal-guides": guidesCollection,
+  blog: postsCollection,
+  resources: resourcesCollection,
 };
